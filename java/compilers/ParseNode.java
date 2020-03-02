15fe1faab70ac63375cdc5ee267d030103e17881
Lucas Kitaev <lkitaev@mymail.mines.edu>
2020-03-02 00:59:10 -0700

    Add basic functionality to ParseTree and ParseNode

--- java/compilers/ParseNode.java
+++ java/compilers/ParseNode.java
@@ -1,9 +1,25 @@
 package compilers;
 
 import java.util.List;
+import java.util.LinkedList;
 
 class ParseNode {
 	private Symbol identifier;
 	private ParseNode parent;
 	private List<ParseNode> children;
+
+	ParseNode(Symbol symbol) {
+		identifier = symbol;
+		parent = null;
+		children = new LinkedList<>();
+	}
+
+	ParseNode getParent() {
+		return parent;
+	}
+
+	void addChild(ParseNode child) {
+		child.parent = this;
+		children.add(child);
+	}
 }
--- java/compilers/ParseTree.java
+++ java/compilers/ParseTree.java
@@ -1,10 +1,43 @@
 package compilers;
 
 import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.ListIterator;
 
 class ParseTree {
 	private ParseNode root;
+	static final Symbol MARKER = new Symbol("*");
+	static final Symbol START = new Symbol("S");
 
-	ParseTree(CFG grammar, ParseTable table, Deque<Token> input) {
+	ParseTree(CFG grammar, ParseTable table, Deque<Token> input) throws Exception {
+		root = new ParseNode();
+		ParseNode cur = root;
+		ArrayDeque<Symbol> symbols = new ArrayDeque<>();
+		symbols.push(START);
+		while (!symbols.isEmpty()) {
+			Symbol s = symbols.pop();
+			if (s.isNonTerminal()) {
+				CFG.Rule rule = table.get(s, input.peek());
+				if (rule = null)
+					throw new Exception();
+				symbols.push(MARKER);
+				List<Symbol> rhs = rule.getRight();
+				ListIterator<Symbol> it = rhs.listIterator(rhs.size());
+				while (it.hasPrevious())
+					symbols.push(it.previous());
+				ParseNode node = new ParseNode(s);
+				cur.addChild(node);
+				cur = node;
+			} else if (s.isTerminal() || s == Symbol.EOF || s == Symbol.LAMBDA) {
+				if (s != Symbol.LAMBDA) {
+					if (!s.matchToken(input.peek()))
+						throw new Exception();
+					input.pop();
+				}
+				cur.addChild(new ParseNode(s));
+			} else if (s == MARKER)
+				cur = cur.getParent();
+		}
 	}
 }
